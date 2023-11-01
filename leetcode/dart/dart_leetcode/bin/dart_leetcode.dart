7760e11bd8b5b782f202195d87d27e2a9c2d2b8f
michael hayes <58147374+michaelhayes1337@users.noreply.github.com>
2023-11-01 10:20:41 +0200

    first commit

---
+++ leetcode/dart/dart_leetcode/bin/dart_leetcode.dart
@@ -0,0 +1,6 @@
+//import 'package:dart_leetcode/dart_leetcode.dart' as dart_leetcode;
+import 'arrays_and_hashing.dart';
+
+void main(List<String> arguments) {
+  print(longestConsecutive128v1([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
+}
