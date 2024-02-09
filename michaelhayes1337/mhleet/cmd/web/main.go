cb6b6d5e1e4966d196d714f9996c1dc9f25f24f3
michael hayes <58147374+michaelhayes1337@users.noreply.github.com>
2024-02-09 21:49:08 +0200

    continuid on my portfolio site

---
+++ michaelhayes1337/mhleet/cmd/web/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"mhleet/pkg/config"
+	"mhleet/pkg/handlers"
+	"mhleet/pkg/render"
+	"net/http"
+)
+
+const portNumber = ":42069"
+
+// main is the main function
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- michaelhayes1337/mhleet/go.mod
+++ michaelhayes1337/mhleet/go.mod
@@ -1,3 +1,7 @@
 module mhleet
 
 go 1.21.6
+
+require github.com/go-chi/chi/v5 v5.0.11
+
+require github.com/justinas/nosurf v1.1.1
