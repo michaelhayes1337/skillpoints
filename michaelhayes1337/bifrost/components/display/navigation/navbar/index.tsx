61b03f21abe6be0e1dbf8225368bbfa13967dd8e
michael hayes <58147374+michaelhayes1337@users.noreply.github.com>
2024-03-09 15:01:57 +0200

    sunset

--- michaelhayes1337/bifrost/components/display/navigation/navbar/index.tsx
+++ michaelhayes1337/bifrost/components/display/navigation/navbar/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Kanit } from "next/font/google";
 import Link from "next/link";
-
+import styles from "./Navbar.module.css";
 const kanit = Kanit({ weight: "700", subsets: ["latin"] });
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const containerClassName = `${styles.NavbarContainer}`;
+  const menuClassName = `${styles.Menu}`;
+  const logoClassName = `${styles.Logo}`;
+  const zClassName = `${styles.Z}`;
   return (
-    <div>
-      <menu>
-        <li>
+    <div className={containerClassName}>
+      <menu className={menuClassName}>
+        <li className={logoClassName}>
           <Link href="/" className={`${kanit.className}`}>
-            skillpointz
+            skillpoint<span className={zClassName}>z</span>
           </Link>
         </li>
         <li>
