0b4a7ba578bc708624d5054e8e05e8d7a84499ad
michael hayes <58147374+michaelhayes1337@users.noreply.github.com>
2024-03-24 19:32:54 +0200

    fuck this

--- 100-days-python/d23/game.py
+++ 100-days-python/d23/game.py
@@ -1,7 +1,7 @@
 from turtle import Turtle, Screen
 import time
 from paddle import Paddle
-
+from ball import Ball
 #Setup gui
 screen = Screen()
 screen_width = 600
@@ -12,25 +12,23 @@ screen.title("Pong Dong")
 screen.tracer(0)
 
 game_on = True
-tick_delay = 0.1
+tick_delay = 0.025
 
 #Setup game objects
 p1 = Paddle(screen_height, -250)
 p2 = Paddle(screen_height, 250)
+ball = Ball(screen_width, screen_height, p1, p2)
 screen.listen()
 
-screen.onkeypress(key="w", fun=p1.start_move_up)
-screen.onkeyrelease(key="w", fun=p1.stop_move_up)
-screen.onkeypress(key="s", fun=p1.start_move_down)
-screen.onkeyrelease(key="s", fun=p1.stop_move_down)
+screen.onkey(key="w", fun=p1.move_up)
+screen.onkey(key="s", fun=p1.move_down)
 
-screen.onkeypress(key="Up", fun=p2.start_move_up)
-screen.onkeyrelease(key="Up", fun=p2.stop_move_up)
-screen.onkeypress(key="Down", fun=p2.start_move_down)
-screen.onkeyrelease(key="Down", fun=p2.stop_move_down)
+screen.onkey(key="Up", fun=p2.move_up)
+screen.onkey(key="Down", fun=p2.move_down)
 
 while game_on:
     time.sleep(tick_delay)
+    ball.move()
     screen.update()
 
 
--- 100-days-python/d23/paddle.py
+++ 100-days-python/d23/paddle.py
@@ -1,43 +1,28 @@
 import turtle
 from turtle import Turtle
 
-rectangle = ((-10, -20), (10, -20), (10, 20), (-10, 20))
-turtle.register_shape("rectangle", rectangle)
-
 
 class Paddle(Turtle):
     def __init__(self, window_height, x_position):
         super().__init__()
         self.shape("square")
-        self.penup()
+        self.shapesize(stretch_wid=1, stretch_len=5)
         self.color("white")
         self.speed("slowest")
         self.setheading(90)
-        self.y_limit = (window_height - 40) / 2
+        self.penup()
         self.setpos(x_position, 0)
-        self.move_up = False
-        self.move_down = False
+        self.y_limit = (window_height - 40) / 2
 
-    def start_move_up(self):
-        self.move_up = True
+    def move_up(self):
         self.move_paddle()
 
-    def stop_move_up(self):
-        self.move_up = False
-        self.move_paddle()
+    def move_down(self):
+        self.move_paddle(False)
 
-    def start_move_down(self):
-        self.move_down = True
-        self.move_paddle()
-
-    def stop_move_down(self):
-        self.move_down = False
-        self.move_paddle()
-
-    def move_paddle(self):
+    def move_paddle(self, up=True):
         x, y = self.pos()
-        if self.move_up and not self.move_down and y < self.y_limit:
-            self.setpos(x, y + 1)
-        if self.move_down and not self.move_up and y > -self.y_limit:
-            self.setpos(x, y + 1)
-        turtle.ontimer(fun=self.move_paddle, t=10)
+        if up and y < self.y_limit - 50:
+            self.setpos(x, y + 25)
+        if not up and y > -self.y_limit + 50:
+            self.setpos(x, y - 25)
